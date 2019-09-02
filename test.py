327dc164d9b7068d9e3cd45f20ea458902a0f9df
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2019-09-01 22:28:29 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print("Hello python")
+import os
+print("Pass my test")
+os.chdir(r"C:\Users\Anish\PycharmProjects\Anish\pyfile")
+os.system("ysic.py")
+os.chdir(r"C:\Users\Anish\PycharmProjects\Anish\jpgfile")
+# Now time for test
+yt = input("What is my youtube name?")
+if yt == 'Technobyte':
+ os.system("texas.jpg")
+ q1 = input("What is this state")
+ if q1 == 'Texas':
+  print("You are smart")
+ else:
+  print(" Game over Enjoy dun dun")
+  os.chdir(r"C:\Users\Anish\PycharmProjects\Anish\pyfile")
+  os.system("start kill.cmd")
+ os.system("start dun.py")
+
+ print("Correct")
+else:
+ os.chdir(r"C:\Users\Anish\PycharmProjects\Anish\pyfile")
+ print(" Game over Enjoy dun dun")
+ os.system("kill.cmd")
+os.system("dun.py")
