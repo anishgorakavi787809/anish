5ca4dff241fad4d324e54998df6202d49b731996
anishgorakavi787809 <34226544+anishgorakavi787809@users.noreply.github.com>
2019-09-01 22:44:42 -0500

    Add files via upload

---
+++ pyfile/dun.py
@@ -0,0 +1,3 @@
+import webbrowser
+print("Haha wrong awnser")
+webbrowser.open("https://www.youtube.com/watch?v=cphNpqKpKc4")
---
+++ pyfile/ysic.py
@@ -0,0 +1,3 @@
+import webbrowser
+print("You have 1 hr to do ")
+webbrowser.open("https://www.youtube.com/watch?v=OnSGk5-GgwY")
